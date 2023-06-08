4a8b19351a8947c7fb87bb7f8a74db36c0d3bfb3
KariVigna <karivigna24@gmail.com>
2023-06-07 20:23:14 -0700

    debugged prompts

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,19 @@
 function add(number1, number2) {
     return number1 + number2;
 }
-function sub(number3, number4) {
-    return number3 - number4;
-
+function sub(number1, number2) {
+    return number1 - number2;
 }
-function multi(number5, number6) {
-    return number5 * number6;
+function multi(number1, number2) {
+    return number1 * number2;
 }
-function div(number7, number8) {
-    return number7 / number8;
+function div(number1, number2) {
+    return number1 / number2;
 }
 
 //user interface logic
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const number3 = parseInt(prompt("Enter a number"));
-const number4 = parseInt(prompt("Another Number"));
-const number5 = parseInt(prompt("Enter a number:"));
-const number6 = parseInt(prompt("Enter another number:"));
-const number7 = parseInt(prompt("Enter a number:"));
-const number8 = parseInt(prompt("Enter another number:"));
+let number1 = parseInt(prompt("Enter a number:"));
+let number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2) + ', ' + sub(number3, number4) + ', ' + multi(number5, number6) + ', ' + div(number7, number8) + '.');
 
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2) + ", " + number1 + " - " + number2 + " = " + sub(number1, number2) + ", " + number1 + " * " + number2 + " = " + multi(number1, number2) + ", " + number1 + " / " + number2 + " = " + div(number1, number2) + ".");
